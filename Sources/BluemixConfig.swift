d606c71788fd8076fb90ddbf6af540be0a968f01
king <king@kings-MacBook-Pro.local>
2017-01-13 15:53:50 -0600

    update approach to get variables

--- Sources/BluemixConfig.swift
+++ Sources/BluemixConfig.swift
@@ -11,11 +11,11 @@ extension ConfigurationManager {
     
     private func loadCloudantConfig () -> BluemixService {
         
-        let username = self.getValue(for: "VCAP_SERVICES:cloudantNoSQLDB:credentials:username") as? String
-        let password = self.getValue(for: "VCAP_SERVICES:cloudantNoSQLDB:credentials:password") as? String
-        let host     = self.getValue(for: "VCAP_SERVICES:cloudantNoSQLDB:credentials:host") as? String
-        let url      = self.getValue(for: "VCAP_SERVICES:cloudantNoSQLDB:credentials:url") as? String
-        let port     = self.getValue(for: "VCAP_SERVICES:cloudantNoSQLDB:credentials:port") as? Int
+        let username = self["VCAP_SERVICES:cloudantNoSQLDB:credentials:username"] as? String
+        let password = self["VCAP_SERVICES:cloudantNoSQLDB:credentials:password"] as? String
+        let host     = self["VCAP_SERVICES:cloudantNoSQLDB:credentials:host"] as? String
+        let url      = self["VCAP_SERVICES:cloudantNoSQLDB:credentials:url"] as? String
+        let port     = self["VCAP_SERVICES:cloudantNoSQLDB:credentials:port"] as? Int
         
         var service = BluemixService()
         service.username = username
