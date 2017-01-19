8bd1b4c639dac864e1c5e5c658511dd9206e8814
king <king@kings-MacBook-Pro.local>
2017-01-19 12:24:33 -0600

    add postgresql support and 2017 copyrights

--- Sources/BluemixService.swift
+++ Sources/BluemixService.swift
@@ -1,3 +1,20 @@
+/*
+ * Copyright IBM Corporation 2017
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
 import Foundation
 
 import CloudFoundryEnv
@@ -27,7 +44,8 @@ public class CloudantService: Service {
                   tags:        service.tags,
                   credentials: service.credentials)
         
-        guard let credentials = credentials, let host = credentials["host"] as? String,
+        guard let credentials = credentials,
+              let host = credentials["host"] as? String,
               let username = credentials["username"] as? String,
               let password = credentials["password"] as? String,
               let port = credentials["port"] as? Int,
@@ -71,6 +89,34 @@ public class RedisService: Service {
         self.host     = host
         self.password = password
         self.port     = port
+        
+    }
+}
+
+public class PostgresqlService: Service {
+    
+    public var public_hostname  : String = ""
+    public var password         : String = ""
+    public var username         : String = ""
+    
+    public init?(withService service: Service) {
+        
+        super.init(name:        service.name,
+                   label:       service.label,
+                   plan:        service.plan,
+                   tags:        service.tags,
+                   credentials: service.credentials)
+        
+        guard let credentials = credentials,
+            let public_hostname = credentials["public_hostname"] as? String,
+            let username = credentials["username"] as? String,
+            let password = credentials["password"] as? String else {
+                return nil
+        }
+        
+        self.public_hostname     = public_hostname
+        self.username = username
+        self.password = password
         
     }
 }
