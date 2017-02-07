3f40f5b604f7b66ed7eead2f4a6d9df1a4316416
Robert F. Dickerson <rfdickerson@us.ibm.com>
2017-02-07 12:03:58 -0600

    Changed name to CloudConfiguration

--- Package.swift
+++ Package.swift
@@ -1,9 +1,9 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftConfiguration",
+    name: "CloudConfiguration",
     dependencies: [
-        .Package(url: "git@github.ibm.com:IBM-Swift/Configuration.git", majorVersion: 0),
+        .Package(url: "https://github.com/IBM-Swift/Configuration", majorVersion: 0),
         .Package(url: "https://github.com/IBM-Swift/Swift-cfenv.git", majorVersion: 2)
     ]
 )
