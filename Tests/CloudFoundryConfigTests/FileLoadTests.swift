3e0c2b229ec72e881371a23e2fded3a83a272059
rolivieri <roliv@us.ibm.com>
2017-02-09 13:20:47 -0600

    test cases were not executing; fixing that; adding support for alert notification service.

--- Tests/CloudFoundryConfigTests/FileLoadTests.swift
+++ Tests/CloudFoundryConfigTests/FileLoadTests.swift
@@ -21,32 +21,32 @@ import Configuration
 
 
 class FileLoadTests: XCTestCase {
-    
+
     static var allTests : [(String, (FileLoadTests) -> () throws -> Void)] {
         return [
             ("testServiceGetters", testServiceGetters),
         ]
     }
-    
+
     func testServiceGetters() {
-        
+
         let manager = ConfigurationManager()
-        
+
         do {
             // Modify relative path for your system, make more dynamic later
-            try manager.load(file: "../../config_example.json",
-                             relativeFrom: .customPath("/Users/tlfrankl/ibm/ibm-swift/bluemix-config/.build/debug"))
-            
+            let filePath = URL(fileURLWithPath: #file).appendingPathComponent("../config_example.json").standardized
+            try manager.load(url: filePath)
+
             let cloudantService = try manager.getCloudantService(name: "CloudantService")
-            
+
             XCTAssertGreaterThan(cloudantService.host.characters.count, 0)
             XCTAssertGreaterThan(cloudantService.username.characters.count, 0)
             XCTAssertGreaterThan(cloudantService.password.characters.count, 0)
             XCTAssertNotEqual(cloudantService.port, 0)
             XCTAssertGreaterThan(cloudantService.url.characters.count, 0)
-            
+
             let mongoDBService = try manager.getMongoDBService(name: "TodoList-MongoDB")
-            
+
             XCTAssertGreaterThan(mongoDBService.host.characters.count, 0)
             XCTAssertGreaterThan(mongoDBService.username.characters.count, 0)
             XCTAssertGreaterThan(mongoDBService.password.characters.count, 0)
@@ -58,34 +58,43 @@ class FileLoadTests: XCTestCase {
             XCTAssertGreaterThan(redisService.host.characters.count, 0)
             XCTAssertGreaterThan(redisService.password.characters.count, 0)
             XCTAssertNotEqual(redisService.port, 0)
-            
+
             let postgreSQLService = try manager.getPostgreSQLService(name: "TodoList-PostgreSQL")
-            
+
             XCTAssertGreaterThan(postgreSQLService.host.characters.count, 0)
             XCTAssertGreaterThan(postgreSQLService.username.characters.count, 0)
             XCTAssertGreaterThan(postgreSQLService.password.characters.count, 0)
             XCTAssertNotEqual(postgreSQLService.port, 0)
-            
+
             let mySQLService = try manager.getMySQLService(name: "TodoList-MySQL")
-            
+
             XCTAssertGreaterThan(mySQLService.database.characters.count, 0)
             XCTAssertGreaterThan(mySQLService.host.characters.count, 0)
             XCTAssertGreaterThan(mySQLService.username.characters.count, 0)
             XCTAssertGreaterThan(mySQLService.password.characters.count, 0)
             XCTAssertNotEqual(mySQLService.port, 0)
-            
+
             let db2Service = try manager.getDB2Service(name: "TodoList-DB2-Analytics")
-            
+
             XCTAssertGreaterThan(db2Service.database.characters.count, 0)
             XCTAssertGreaterThan(db2Service.host.characters.count, 0)
             XCTAssertNotEqual(db2Service.port, 0)
             XCTAssertGreaterThan(db2Service.uid.characters.count, 0)
             XCTAssertGreaterThan(db2Service.pwd.characters.count, 0)
-            
+
+            let alertNotificationService = try manager.getAlertNotificationService(name: "IBM Alert Notification-xs")
+
+            //XCTAssertEqual(alertNotificationService.url, "https://ibmnotifybm.mybluemix.net/api/alerts/v1", "Alert Notification Service URL should match.")
+            //XCTAssertEqual(alertNotificationService.id, "21a084f4-4eb3-4de4-9834-33bdc7be5df9/d2a85740-da7a-4615-aabf-5bdc35c63618", "Alert Notification Service ID should match.")
+            //XCTAssertEqual(alertNotificationService.password, "alertnotification-pwd, "Alert Notification Service password should match.")
+            //XCTAssertEqual(alertNotificationService.swaggerUI, "https://ibmnotifybm.mybluemix.net/docs/alerts/v1", "Alert Notification Service swaggerUI should match.")
+
+
+
         } catch {
             XCTFail("Could not load configuration. Error: \(error)")
         }
-        
+
     }
-    
+
 }
