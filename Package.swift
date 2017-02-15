57076806bfaf0525e0d7ecdd9fca5e8e277df553
rolivieri <roliv@us.ibm.com>
2017-02-15 17:57:14 -0600

    Migrated code to use new version of Swift-cfenv.

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ let package = Package(
     name: "CloudConfiguration",
     dependencies: [
         .Package(url: "https://github.com/IBM-Swift/Configuration", majorVersion: 0),
-        .Package(url: "https://github.com/IBM-Swift/Swift-cfenv.git", majorVersion: 2)
+        .Package(url: "https://github.com/IBM-Swift/Swift-cfenv.git", majorVersion: 100)
     ]
 )
