e672696ea94d5bb0bf0038b1094cfea45831fd91
rolivieri <roliv@us.ibm.com>
2017-02-15 20:16:13 -0600

    Updated package.swift

--- Package.swift
+++ Package.swift
@@ -3,7 +3,6 @@ import PackageDescription
 let package = Package(
     name: "CloudConfiguration",
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Configuration", majorVersion: 0),
         .Package(url: "https://github.com/IBM-Swift/Swift-cfenv.git", majorVersion: 3)
     ]
 )
