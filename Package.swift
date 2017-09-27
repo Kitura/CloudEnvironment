f60c9664f8a2c92e265cb28e46dc87c826b62c12
Aaron Liberatore <aaron.m.liberatore@ibm.com@aarons-mbp.austin.ibm.com>
2017-09-27 13:59:36 -0500

    Updates logger-api to c2.0.0

--- Package.swift
+++ Package.swift
@@ -29,7 +29,7 @@ let package = Package(
     ],
     dependencies: [
       .package(url: "https://github.com/IBM-Swift/Swift-cfenv.git", .upToNextMajor(from: "5.0.0")),
-      .package(url: "https://github.com/IBM-Swift/LoggerAPI.git", .upToNextMajor(from: "1.0.0"))
+      .package(url: "https://github.com/IBM-Swift/LoggerAPI.git", .upToNextMajor(from: "2.0.0"))
     ],
     targets: [
       .target(
