266ffc6b542eccd0fa9f9384c437b1c83e4197e0
Aaron Liberatore <aml68@duke.edu>
2017-09-29 14:37:09 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -23,6 +23,6 @@ let package = Package(
   ],
   dependencies: [
     .Package(url: "https://github.com/IBM-Swift/LoggerAPI.git", majorVersion: 1),
-    .Package(url: "https://github.com/IBM-Swift/Swift-cfenv.git", majorVersion: 4,)
+    .Package(url: "https://github.com/IBM-Swift/Swift-cfenv.git", majorVersion: 4)
   ]
 )
