95dc9997de623c89526e160301387e5ed4964714
Aaron Liberatore <AaronLiberatore@aaron.attlocal.net>
2017-11-09 18:09:57 -0600

    Cloud functions support

---
+++ Tests/CloudEnvironmentTests/CloudFunctionsCredentialsTests.swift
@@ -0,0 +1,45 @@
+/*
+ * Copyright IBM Corporation 2017
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import XCTest
+import Configuration
+@testable import CloudEnvironment
+
+class CloudFunctionsTests: XCTestCase {
+
+    static var allTests : [(String, (CloudFunctionsTests) -> () throws -> Void)] {
+        return [
+            ("testGetCredentials", testGetCredentials),
+        ]
+    }
+
+    func testGetCredentials() {
+
+        // Load test mappings.json file and Cloud Foundry test credentials-- VCAP_SERVICES and VCAP_APPLICATION
+        let cloudEnv = CloudEnv(mappingsFilePath: "Tests/CloudEnvironmentTests/resources", cloudFoundryFile: "Tests/CloudEnvironmentTests/resources/config_cf_example.json")
+
+        guard let credentials =  cloudEnv.getCloudFunctionsCredentials(name: "CloudFunctionsCredentials") else {
+            XCTFail("Could not load Alert Notification service credentials.")
+            return
+        }
+
+        let computedAuthToken = "f4a7ebaf".data(using: String.Encoding.utf8)?.base64EncodedString(options: Data.Base64EncodingOptions(rawValue: 0))
+
+        XCTAssertEqual(credentials.hostName, "openwhisk.ng.bluemix.net", "Cloud Functions host should match.")
+        XCTAssertEqual(credentials.urlPath, "/path", "Cloud Functions url path should match.")
+        XCTAssertEqual(credentials.authToken, computedAuthToken, "Cloud Functions auth token should match.")
+    }
+}
