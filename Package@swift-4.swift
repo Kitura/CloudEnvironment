9502ee0f4fe8cb6ea414d08cf69cfff932cbdc6b
Fouad Hatem <fouadhatem@icloud.com>
2020-09-28 16:42:43 +0300

    Update copyright and package reference

--- Package@swift-4.swift
+++ Package@swift-4.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:4.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 /**
- * Copyright IBM Corporation 2016, 2017
+ * Copyright IBM Corporation and the Kitura project authors 2016-2020
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -27,7 +27,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/IBM-Swift/Swift-cfenv.git", .upToNextMajor(from: "6.0.0")),
+        .package(url: "https://github.com/Kitura/Swift-cfenv.git", .upToNextMajor(from: "6.0.0")),
     ],
     targets: [
         .target(
