498d60a5078b4e00153b4f5adca8b5b31075f08b
Danny Sung <danny@dannysung.com>
2020-10-07 19:59:31 -0700

    Update package reference to community versions (#64)

--- Package.swift
+++ Package.swift
@@ -27,7 +27,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/Kitura/Swift-cfenv.git", .upToNextMajor(from: "6.0.0")),
+        .package(url: "https://github.com/Kitura/Swift-cfenv.git", .upToNextMajor(from: "6.0.200")),
     ],
     targets: [
         .target(
